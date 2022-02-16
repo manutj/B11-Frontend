880ced0efa978c063c4b92a365cbf157912f05fc
Manuel Tejada <manuel.tejada95@outlook.com>
2022-02-15 21:52:57 -0600

    Add: Uso de cleaner de UseEffect en paginacion de datos

--- airbnb_page/src/hooks/useEffect.js
+++ airbnb_page/src/hooks/useEffect.js
@@ -1,46 +1,99 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import Hello from "../components/Hello";
 
 export default function UseEffectHook(props) {
-    const [count, setCount] = useState(0);
-    const [data, setData] = useState();
-    const [show, setShow] = useState(true);
+    // const [count, setCount] = useState(0);
+    // const [data, setData] = useState();
+    // const [show, setShow] = useState(true);
+
+    const [users, setUsers] = useState([]);
+    const [user, setUser] = useState({});
+    const [currentId, setCurrentId] = useState(1);
+
+    const controller = new AbortController();
+
+    // useEffect(() => {
+    //     // AQUI PODEMOS VISUALIZAR EL OBJETO DEL PERSONAJE QUE TRAE LA PROMESA PERO NO PODEMOS ACCEDER A SUS PROPIEDADES
+
+    //     const getData = async () => {
+    //         const response = await fetch(
+    //             `https://swapi.dev/api/people/${props.id}`
+    //         );
+    //         const newData = response.json();
+    //         setData(newData);
+    //     };
+
+    //     getData();
+
+    //     // PARA ACCEDER A LOS DATOS DEL OBJETO DEL PERSONAJE, DEBEMOS MAEJARLO COMO PROMESA
+    //     fetch(`https://swapi.dev/api/people/${props.id}`)
+    //         .then((response) => response.json())
+    //         .then((data) => setData(data));
+    // }, [props.id]);
 
     useEffect(() => {
-        //AQUI PODEMOS VISUALIZAR EL OBJETO DEL PERSONAJE QUE TRAE LA PROMESA PERO NO PODEMOS ACCEDER A SUS PROPIEDADES
+        const endpoint = "https://jsonplaceholder.typicode.com/todos";
+        axios
+            .get(endpoint, {
+                signal: controller.signal,
+            })
+            .then(({ data }) => setUsers(data));
+        return () => controller.abort();
+    }, []);
 
-        // const getData = async () => {
-        //     const response = await fetch(
-        //         `https://swapi.dev/api/people/${props.id}`
-        //     );
-        //     const newData = response.json();
-        //     setData(newData);
-        // };
+    useEffect(() => {
+        const endpoint = "https://jsonplaceholder.typicode.com/todos";
+        axios
+            .get(`${endpoint}/${currentId}`, {
+                signal: controller.signal,
+            })
+            .then(({ data }) => setUser(data))
+            .then(() => user.completed.toString());
 
-        // getData();
+        return () => controller.abort();
+    }, [currentId]);
 
-        //PARA ACCEDER A LOS DATOS DEL OBJETO DEL PERSONAJE, DEBEMOS MAEJARLO COMO PROMESA
-        fetch(`https://swapi.dev/api/people/${props.id}`)
-            .then((response) => response.json())
-            .then((data) => setData(data));
-    }, [props.id]);
+    // users && console.log(users);
+    user && console.log(user);
 
-    data && console.log(data);
+    // data && console.log(data);
+
+    const Previous = () => {
+        setCurrentId((currentId) =>
+            currentId === 1 ? currentId : currentId - 1
+        );
+    };
+
+    const Next = () => {
+        setCurrentId((currentId) =>
+            currentId < users.length ? currentId + 1 : currentId
+        );
+    };
 
     return (
-        <div>
-            {/* <h1>Hola</h1> */}
-            <h1>{count}</h1>
-            <button onClick={() => setCount((c) => c + 1)}>+</button>
-            {data ? (
-                <div>{`Nombre del personaje:  ${data.name}`}</div>
-            ) : (
-                <div>no hay datos</div>
-            )}
-            <button onClick={() => setShow(!show)}>Toggle</button>
+        // <div>
+        //     {/* <h1>Hola</h1> */}
+        //     <h1>{count}</h1>
+        //     <button onClick={() => setCount((c) => c + 1)}>+</button>
+        //     {data ? (
+        //         <div>{`Nombre del personaje:  ${data.name}`}</div>
+        //     ) : (
+        //         <div>no hay datos</div>
+        //     )}
+        //     <button onClick={() => setShow(!show)}>Toggle</button>
 
-            {/* COMPONENTE QUE MONTAMOS Y DESMONTAMOS CON USE EFFECT Y USE STATE */}
-            {show && <Hello />}
+        //     {/* COMPONENTE QUE MONTAMOS Y DESMONTAMOS CON USE EFFECT Y USE STATE */}
+        //     {show && <Hello />}
+        // </div>
+        <div>
+            <div>
+                <h2>{user.title}</h2>
+                <p>Occupped: {user.completed}</p>
+
+                <button onClick={Previous}>Anterior</button>
+                <button onClick={Next}>Siguiente</button>
+            </div>
         </div>
     );
 }
