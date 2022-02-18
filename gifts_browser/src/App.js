1c77ef7ca3cea74de49055f799050229067e484c
Manuel Tejada <manuel.tejada95@outlook.com>
2022-02-17 22:06:10 -0600

    Add: Buscador de gifts con react y GIPHY

---
+++ gifts_browser/src/App.js
@@ -0,0 +1,11 @@
+import { Home } from "./components/Home";
+function App() {
+    return (
+        <div>
+            <h1>Aplicacion de busqueda de gifts</h1>
+            <Home />
+        </div>
+    );
+}
+
+export default App;
