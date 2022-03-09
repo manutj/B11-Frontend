e81a2790df1e339ba6639c2d85a677dc64aea7bb
Manuel Tejada <manuel.tejada95@outlook.com>
2022-03-08 21:34:52 -0600

    Add: Uso de Sass y BEM

--- atomic_design/src/App.js
+++ atomic_design/src/App.js
@@ -1,8 +1,12 @@
 import "./Styles/App.css";
-import Home from "./Components/templates/Home/Home";
+// import Home from "./Components/templates/Home/Home";
+// import Button from "./Components/atoms/Buttons/Button";
+// import Nav from "./Components/organisms/Nav/Nav";
+import Card from "./Components/organisms/card/Card";
 
 function App() {
-    return <Home />;
+    return <Card />;
+    // return <Home />;
 }
 
 export default App;
